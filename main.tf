8d45fdbe474b741e544756eced990dbfb36a70d7
kandalajayanthkumarnaidu <jayanthkumarnaidukandala@gmail.com>
2025-07-31 11:21:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "ap-south-1"
 
 resource "aws_instance" "one" {
 count = 5
-ami = "ami-0d0ad8bb301edb745 "
+ami = "ami-0d0ad8bb301edb745"
 instance_type = "t2.medium"
 key_name = "jai.pem"
 vpc_security_group_ids = ["sg-01fe2eaa0522b07b9"]
