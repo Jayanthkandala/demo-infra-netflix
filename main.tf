790db998f4bc30093190756856144023b8335691
kandalajayanthkumarnaidu <jayanthkumarnaidukandala@gmail.com>
2025-07-31 12:02:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 5
 ami = "ami-0d0ad8bb301edb745"
 instance_type = "t2.medium"
 key_name = "project"
-vpc_security_group_ids = ["sg-01fe2eaa0522b07b9"]
+vpc_security_group_ids = ["sg-0e45296f21ec7c07b"]
 tags = {
 Name = var.instance_names[count.index]
 }
