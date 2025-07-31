81b4cb27ecfd1eb97e57e00a878e139c3e1bf32c
kandalajayanthkumarnaidu <jayanthkumarnaidukandala@gmail.com>
2025-07-31 11:33:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
 count = 5
 ami = "ami-0d0ad8bb301edb745"
 instance_type = "t2.medium"
-key_name = "project.pem"
+key_name = "project"
 vpc_security_group_ids = ["sg-01fe2eaa0522b07b9"]
 tags = {
 Name = var.instance_names[count.index]
